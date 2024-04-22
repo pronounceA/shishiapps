4206c3e55212b1c8cf47065d459e938ea76a864b
pronounceA <zyank.wpo.zyank.wpo@gmail.com>
2024-04-23 00:10:57 +0900

    add test

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Delete from './pages/Delete';
 import Home from './pages/Home';
 import OverWatch from './pages/OverWatch';
 import CollectDaruma from './pages/CollectDaruma';
+import B from './pages/B';
 
 import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
+  /*
+  //メニューの開閉を管理。
+  */
   const [isOpen, setOpen] = useState(false);
 
   return (
@@ -26,6 +30,7 @@ function App() {
             <li className='menu-list' onClick={() => setOpen(false)}><Link to="/">home</Link></li>
             <li className='menu-list' onClick={() => setOpen(false)}><Link to="/TicTacToe">TicTacToe</Link></li>
             <li className='menu-list' onClick={() => setOpen(false)}><Link to="/CollectDaruma">だるまあつめ</Link></li>
+            <li className='menu-list' onClick={() => setOpen(false)}><Link to="/B">B</Link></li>
           </ul>
         </header>
       </div>
@@ -35,6 +40,7 @@ function App() {
           <Route exact path="/TicTacToe" element={ <TicTacToe /> } /> 
           <Route exact path="/CollectDaruma" element={ <CollectDaruma />} />
           <Route exact path="/ow2" element={ <OverWatch />}></Route>
+          <Route exact path="/B" element={ <B />}></Route>
           <Route path="*" element={<Delete />} />
         </Routes>
       </div>
--- src/pages/B.jsx
+++ src/pages/B.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 
 const B = () => {
+
+	const [state, setState] = useState('');
+
+	const testfunc = () => {
+		setState('a');
+		setState('b');
+	};
+
 	return (
 		<div>
-			<p>B</p>
+			<button onClick={() => testfunc()}>a</button>
+			<p>{state}</p>
 		</div>
 	)
 }
