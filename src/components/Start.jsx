19fa088a468fa89e06c6061a376c7988bb111e66
pronounceA <zyank.wpo.zyank.wpo@gmail.com>
2024-04-28 13:36:14 +0900

    debug

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,10 +1,8 @@
-import { useState, useEffect } from 'react'
-import MyButton from './MyButton'
-import { useState, useEffect } from 'react'
-import MyButton from './MyButton'
+import { useState, useEffect } from 'react';
+import MyButton from './MyButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Alert from 'react-bootstrap/Alert';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 import Modal from 'react-bootstrap/Modal';
 import { Button } from 'react-bootstrap';
